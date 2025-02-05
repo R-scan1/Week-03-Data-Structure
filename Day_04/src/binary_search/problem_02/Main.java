cfd2417e5007f9ffedfd4fa1f0a6e6c6ac1b5396
R-scan1 <deepr7590@gmail.com>
2025-02-05 22:20:32 +0530

    [Raj] Add. adding binary_search problem_02

---
+++ Day_04/src/binary_search/problem_02/Main.java
@@ -0,0 +1,10 @@
+package binary_search.problem_02;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr={10,20,7,19,90};
+        int result =PeakElement.findFirstPeak(arr);
+        System.out.println("First Peak Element :"+result);
+
+    }
+}
---
+++ Day_04/src/binary_search/problem_02/PeakElement.java
@@ -0,0 +1,25 @@
+package binary_search.problem_02;
+
+public class PeakElement {
+    public static int findFirstPeak(int[] arr)
+    {
+        int n=arr.length-1;
+        int left=0;
+        int right=n-1;
+        while(left!=right)
+        {
+            int mid= (left+right)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return arr[mid];
+            }
+            else if(arr[mid]<arr[mid+1])
+            {
+                left=mid+1;
+            }
+            else {
+                right=mid-1;
+            }
+        }
+        return -1;
+    }
+}
